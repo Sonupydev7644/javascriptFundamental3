7ef7ef2bc5209c9167ed2852a2ecaf110c5cab5a
Sonu kumar <164761008+Sonupydev7644@users.noreply.github.com>
2024-06-29 14:07:30 +0530

    Add files via upload

---
+++ 3question.js
@@ -0,0 +1,10 @@
+// Function to return the number of properties in an object
+function countProperties(obj) {
+    return Object.keys(obj).length;
+  }
+  
+  const exampleObject = { a: 1, b: 2, c: 3 };
+  console.log(countProperties(exampleObject));
+  
+  
+  
